a1782f3fe382fce004c0fae50763a135923f555f
ZXY101 <tennershaun@gmail.com>
2024-02-20 22:39:40 +0200

    Initial commit - project setup & testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,209 @@
-import Image from "next/image";
+'use client';
+
+import * as THREE from 'three';
+import { createRef, forwardRef, useEffect, useMemo, useRef } from 'react';
+import {
+  Canvas,
+  ReactThreeFiber,
+  extend,
+  useFrame,
+  useLoader,
+  useThree,
+} from '@react-three/fiber';
+import {
+  CameraControls,
+  OrbitControls,
+  ScrollControls,
+  Sky,
+  Stars,
+  useCamera,
+  useGLTF,
+  useGizmoContext,
+  useScroll,
+  Html,
+  Scroll,
+  Clouds,
+  Cloud,
+  Environment,
+  Shadow,
+} from '@react-three/drei';
+import { Water } from 'three-stdlib';
+import { Leva, buttonGroup, useControls, monitor } from 'leva';
+import gsap from 'gsap';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+import Tori from './Tori';
+import { useGSAP } from '@gsap/react';
+import { DEG2RAD } from 'three/src/math/MathUtils.js';
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      water: ReactThreeFiber.Object3DNode<Water, typeof Water>;
+    }
+  }
+}
+
+extend({ Water });
+/*
+{"y":60}
+{"rayleigh":5.5}
+{"turbidity":4.34}
+{"distance":1300}
+{"mieCoefficient":0.01}
+{"mieDirectionalG":0.98}
+*/
+
+function Ocean() {
+  const ref = useRef<any>();
+  const gl = useThree((state) => state.gl);
+  const waterNormals = useLoader(THREE.TextureLoader, '/waternormals.jpeg');
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), []);
+
+  const config = useMemo(
+    () => ({
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals,
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 'rgba(0, 122, 160, 0.31)',
+      distortionScale: 3.7,
+      fog: false,
+    }),
+    [waterNormals]
+  );
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.material.uniforms.time.value += delta * 0.3;
+    }
+  });
+  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />;
+}
+
+const SkyView = forwardRef(function SkyView(props: any, ref) {
+  return (
+    <>
+      <Sky {...props} ref={ref} />
+      <CloudCollection />
+    </>
+  );
+});
+
+function Scene() {
+  const {
+    x,
+    y,
+    z,
+    rayleigh,
+    turbidity,
+    distance,
+    mieCoefficient,
+    mieDirectionalG,
+  } = useControls({
+    x: { value: 0, min: -5000, max: 5000 },
+    y: { value: -2.8, min: -100, max: 100, step: 0.1 },
+    z: { value: -2000, min: -2000, max: 200 },
+    rayleigh: { value: 3.95, min: 0, max: 10, step: 0.01 },
+    turbidity: { value: 6.2, min: 0, max: 20, step: 0.01 },
+    distance: { value: 1300, min: 0, max: 10000 },
+    mieCoefficient: { value: 0.01, min: 0, max: 1, step: 0.01 },
+    mieDirectionalG: { value: 0.95, min: 0, max: 1, step: 0.01 },
+  });
+
+  const scroll = useScroll();
+  const tl = useRef<gsap.core.Timeline>();
+  const sky = useRef<any>();
+  const { camera } = useThree();
+
+  useFrame(() => {
+    tl.current?.seek(scroll.offset * tl.current.duration());
+  });
+
+  console.log(sky.current);
+
+  useGSAP(() => {
+    tl.current = gsap.timeline();
+    tl.current.add('start');
+    tl.current.to(
+      sky.current.material.uniforms.rayleigh,
+      { value: 0 },
+      'start'
+    );
+    tl.current.to(
+      sky.current.material.uniforms.sunPosition.value,
+      {
+        y: 0,
+      },
+      'start'
+    );
+    // tl.current.to(camera.rotation, { y: Math.PI, x: 0 });
+    // tl.current.to(camera.position, { y: 20, z: 50 });
+  });
+
+  return (
+    <>
+      <Ocean />
+      <ambientLight intensity={1} />
+      <SkyView
+        ref={sky}
+        sunPosition={[x, y, z]}
+        rayleigh={rayleigh}
+        turbidity={turbidity}
+        distance={distance}
+        mieCoefficient={mieCoefficient}
+        mieDirectionalG={mieDirectionalG}
+      />
+      <Tori />
+    </>
+  );
+}
+
+function CloudCollection() {
+  const ref = useRef<any>();
+  const { cx, cy, cz, ...config } = useControls('Clouds', {
+    cx: { value: 6, min: 0, max: 100, step: 1 },
+    cy: { value: 1, min: 0, max: 100, step: 1 },
+    cz: { value: 1, min: 0, max: 100, step: 1 },
+    seed: { value: 1, min: 1, max: 100, step: 1 },
+    segments: { value: 20, min: 1, max: 80, step: 1 },
+    volume: { value: 6, min: 0, max: 100, step: 0.1 },
+    opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
+    fade: { value: 10, min: 0, max: 400, step: 1 },
+    growth: { value: 4, min: 0, max: 20, step: 1 },
+    speed: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    color: 'white',
+  });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.y = Math.cos(state.clock.elapsedTime / 2) / 2;
+    ref.current.rotation.x = Math.sin(state.clock.elapsedTime / 2) / 2;
+  });
+  return (
+    <group ref={ref}>
+      <Clouds material={THREE.MeshLambertMaterial}>
+        <Cloud {...config} bounds={[cx, cy, cz]} />
+      </Clouds>
+    </group>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <Canvas
+        camera={{
+          position: [0, 1.5, 15],
+          rotation: [5 * DEG2RAD, 0, 0],
+          fov: 60,
+        }}
+        dpr={window.devicePixelRatio}
+      >
+        <ScrollControls pages={2}>
+          <Scene />
+        </ScrollControls>
+      </Canvas>
     </main>
   );
 }
